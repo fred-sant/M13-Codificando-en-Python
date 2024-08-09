6fab6b984015c6b2eb0697c1d93294f14290540c
Fredy Santander <fredy.santander@fredys-macbook-pro.local>
2024-08-09 12:18:26 -0600

    Estructura inicial

---
+++ HolaMundo.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Aug  8 12:39:53 2024
+
+@author: fredy.santander
+"""
+print("Hola Mundo")
---
+++ sum.py
@@ -0,0 +1,12 @@
+# ********************
+# LA SUMA MÁS SENCILLA
+# ********************
+
+def run(a: int, b: int) -> int:
+    # Realiza la suma de a y b
+    result = a + b
+    return result
+
+if __name__ == '__main__':
+    # Llama a la función y muestra el resultado
+    print(run(6, 4))  # Esto imprimirá 7
